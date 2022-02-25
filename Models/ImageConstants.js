b31922dad71465829851bc151a59d7100a42fe14
Deepika Gupta <its.deepika08@gmail.com>
2022-02-25 12:33:52 +0530

    Add image constants

---
+++ Models/ImageConstants.js
@@ -0,0 +1,9 @@
+const FoodOffer = ".Images/Food_Offer.png";
+const HeaderLogo = "./Images/Header_Logo.png";
+const VegLogo = "./Images/VegLogo.png";
+const MixedPlatter = ".Images/Dishes/Platters/Mixedplatter.png";
+const VegPlatter = ".Images/Dishes/Platters/VegPlatter.png";
+const ChilliGarlicNoodles = ".Images/Dishes/Recommended/ChilliGarlicNoodles.png";
+const AchariPaneerTikka = ".Images/Dishes/Starters/AchariPaneerTikka.png";
+const PaneerMalaiTikka = ".Images/Dishes/Starters/PaneerMalaiTikka.png";
+export { FoodOffer, HeaderLogo, VegLogo, MixedPlatter, VegPlatter, ChilliGarlicNoodles, AchariPaneerTikka, PaneerMalaiTikka };
