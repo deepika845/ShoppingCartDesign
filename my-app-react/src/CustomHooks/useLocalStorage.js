262b6d3d7fe825644c9c542f5de9bca6d8a5531e
Deepika Gupta <its.deepika08@gmail.com>
2022-03-23 19:10:35 +0530

    Removed unwanted files

--- my-app-react/src/CustomHooks/useLocalStorage.js
+++
@@ -1,9 +0,0 @@
-import { useState } from "react";
-
-function useLocalStorage(initialItem, initialValue) {
-    const [initialState, setState] = useState({ initialItem: initialValue });
-    localStorage.setItem(initialItem, initialValue);
-    function setLocalStorageState(param) {
-        localStorage.getItem()
-    }
-}
